641965a59d237c0bde1d8b8588e44d90677a9660
Karkuvel Pandi <karkuvelpandi007@gmail.com>
2023-08-29 10:25:39 +0530

    fix: :recycle: fixes footer responsiveness

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -8,21 +8,10 @@ import { CategoryBar } from "./components/CategoryBar";
 import { SearchBar } from "./components/SearchBar";
 
 const Navbar = () => {
-  const [scrolled, setScrolled] = useState(false);
   //
   const isMobileView = useSelector(
     (state: RootState) => state.visibility.isMobileView
   );
-  //
-  const scrollToActive = () => {
-    if (window.scrollY >= 75) {
-      setScrolled(true);
-    } else {
-      setScrolled(false);
-    }
-  };
-  window.addEventListener("scroll", scrollToActive);
-  //
 
   return (
     <>
